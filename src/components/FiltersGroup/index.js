f54bb4266a9caa6476e50aab60db2150f5f1ff45
Harivaradhan KM <harivaradhank@gmail.com>
2023-08-30 08:33:47 +0000

    midupdate34

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const FiltersGroup = () => (
+  <div className="filters-group-container">
+    <h1>Filters Group</h1>
+    {/* Replace this element with your code */}
+  </div>
+)
+
+export default FiltersGroup
